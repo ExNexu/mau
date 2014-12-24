b1bd55679bcce58a77c2cc38f96b3822252c4364
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-12-24 13:23:44 -0800

    Add save/get test for MauDatabaseRedis

---
+++ src/test/scala/mau/mauredis/MauDatabaseRedisTest.scala
@@ -0,0 +1,59 @@
+package mau.mauredis
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Future
+
+import akka.actor.ActorSystem
+import mau._
+import mau.mauspray._
+import mau.test._
+import redis.RedisClient
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+
+class MauDatabaseRedisTest extends MauSpec {
+  import PersonProtocol._
+
+  val x = (personMauStrategy, personJsonFormat) // TODO: Implicit resolution, meh
+
+  describe("MauDatabaseRedis") {
+
+    it("should save and get an object") {
+      val person1 = Person(None, "Thomas")
+      val savedObject = await(mauDatabaseRedis.save(person1))
+      val id = savedObject.id.get
+      val getObject = await(mauDatabaseRedis.get[Person](id))
+      Some(savedObject) should be(getObject)
+      person1.name should be(getObject.get.name)
+    }
+
+  }
+
+  implicit val actorSystem = ActorSystem("MauDatabaseRedisTestSystem")
+  val redisClient = RedisClient()
+  val namespace = "Mau:Test:MauDatabaseRedisTest"
+  val mauDatabaseRedis = new MauDatabaseRedis(redisClient, namespace) {
+    override protected def remove[A <: Model[A]: MauStrategy](id: Id): Future[Int] = Future.successful(0)
+
+    override protected def addToKey(id: Id, key: Key): Future[Int] = Future.successful(0)
+
+    override protected def removeFromKey(id: Id, key: Key): Future[Int] = Future.successful(0)
+
+    override protected def getPureKeyContent[A <: Model[A]: MauStrategy: MauDeSerializer](key: Key): Future[List[A]] = Future.successful(Nil)
+  }
+
+}
+
+case class Person(id: Option[Id], name: String) extends Model[Person] {
+  override def withId(id: Id) = copy(id = Some(id))
+}
+
+object PersonProtocol {
+  implicit val personJsonFormat = jsonFormat2(Person.apply)
+
+  implicit object personMauStrategy extends MauStrategy[Person] {
+    override val typeName = "Person"
+    override def getKeys(obj: Person): List[Key] = Nil
+  }
+}
+
