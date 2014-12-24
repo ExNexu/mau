0774eb58fccbfddf4c55576ab1819285168c58b1
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-12-24 11:23:56 -0800

    Prototype implement persist in MauDatabaseRedis

--- src/main/scala/mau/mauredis/MauRedisDatabase.scala
+++ src/main/scala/mau/mauredis/MauRedisDatabase.scala
@@ -2,6 +2,7 @@ package mau.mauredis
 
 import scala.concurrent.ExecutionContext
 import scala.concurrent.Future
+import java.util.UUID
 
 import mau._
 import redis.RedisClient
@@ -23,7 +24,18 @@ class MauDatabaseRedis(
     }
   }
 
-  override protected def persist[T <: Model: MauStrategy: MauSerializer](obj: T): Future[T] = ???
+  override protected def persist[T <: Model: MauStrategy: MauSerializer](obj: T): Future[T] = {
+    val mauStrategy = implicitly[MauStrategy[T]]
+    val mauSerializer = implicitly[MauSerializer[T]]
+
+    val id = obj.id.getOrElse(generateId())
+    val objWithId = obj.id.fold(obj.withId(id))(_ ⇒ obj)
+    val serializedObj = mauSerializer.serialize(objWithId)
+    val key = longKeyForId(id, mauStrategy.typeName)
+
+    val saveResult = client.set(key, serializedObj) // TODO: What does the Boolean mean?
+    saveResult map (_ ⇒ objWithId)
+  }
 
   override protected def remove[T <: Model: MauStrategy](id: Id): Future[Int] = ???
 
@@ -34,7 +46,10 @@ class MauDatabaseRedis(
   override protected def getPureKeyContent[T <: Model: MauStrategy: MauDeSerializer](key: Key): Future[List[T]] = ???
 
   private def longKey(key: Key, typeName: String) = s"$namespace:$typeName:$key"
+
   private def longKeyForId(id: Id, typeName: String) = longKey(id, typeName)
+
+  private def generateId(): Id = UUID.randomUUID().toString
 
 }
 
