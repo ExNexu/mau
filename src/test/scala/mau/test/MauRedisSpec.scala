45e9e530846d3fceaf9e71197bb8b3d6b6fc0b6c
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-12-24 13:38:29 -0800

    Fix MauRedisSpec to really delete keys

--- src/test/scala/mau/test/MauRedisSpec.scala
+++ src/test/scala/mau/test/MauRedisSpec.scala
@@ -11,10 +11,8 @@ abstract class MauRedisSpec(val testName: String) extends MauSpec {
   val namespace = s"Mau:Test:$testName"
 
   override def afterEach() {
-    val keys = redisClient.keys(namespace)
-    await(
-      keys map redisClient.del
-    )
+    val keys = redisClient.keys(s"$namespace*")
+    await(keys map redisClient.del)
   }
 
   override def afterAll() {
