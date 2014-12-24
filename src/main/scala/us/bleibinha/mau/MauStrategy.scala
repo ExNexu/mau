e5fc06010a384fa8d27bc65ccbe83f75bff02e71
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-12-23 20:51:50 -0800

    adds ideas to MauStrategy

--- src/main/scala/us/bleibinha/mau/MauStrategy.scala
+++ src/main/scala/us/bleibinha/mau/MauStrategy.scala
@@ -1,6 +1,9 @@
 package us.bleibinha.mau
 
+// TODO: Require JsonFormat here and remove from MauDatabase
 trait MauStrategy[T <: Model] {
+
+  def typeName: String
 
   def getKeys(obj: T): List[Key]
 }
