6c37aa03ceffc3778c479c8710f4f69899b2f4ed
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2014-12-24 11:03:08 -0800

    Add prototype implementation of get in MauRedisDatabase

--- src/main/scala/mau/mauredis/MauRedisDatabase.scala
+++ src/main/scala/mau/mauredis/MauRedisDatabase.scala
@@ -7,13 +7,26 @@ import mau._
 import redis.RedisClient
 import spray.json.JsonReader
 import spray.json.JsonWriter
+import spray.json.JsValue
+import spray.json.JsonParser
 
 class MauDatabaseRedis(
   protected val client: RedisClient,
   protected val namespace: String,
   override protected implicit val ec: ExecutionContext) extends MauDatabase {
 
-  override def get[T <: Model: MauStrategy: JsonReader](id: Id): Future[Option[T]] = ???
+  override def get[T <: Model: MauStrategy: JsonReader](id: Id): Future[Option[T]] = {
+    val mauStrategy = implicitly[MauStrategy[T]]
+    val key = longKeyForId(id, mauStrategy.typeName)
+    val json: Future[Option[String]] = client.get[String](key)
+    json map { json ⇒
+      json map { json ⇒
+        val jsonJsValue = JsonParser(json)
+        val jsonReader = implicitly[JsonReader[T]]
+        jsonReader.read(jsonJsValue)
+      }
+    }
+  }
 
   override protected def persist[T <: Model: MauStrategy: JsonWriter](obj: T): Future[T] = ???
 
@@ -24,6 +37,9 @@ class MauDatabaseRedis(
   override protected def removeFromKey(id: Id, key: Key): Future[Int] = ???
 
   override protected def getPureKeyContent[T <: Model: MauStrategy: JsonReader](key: Key): Future[List[T]] = ???
+
+  private def longKey(key: Key, typeName: String) = s"$namespace:$typeName:$key"
+  private def longKeyForId(id: Id, typeName: String) = longKey(id, typeName)
 
 }
 
