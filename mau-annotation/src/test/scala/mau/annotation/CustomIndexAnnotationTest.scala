4fafec44e375f8c825537397954219b54d5b717d
Stefan Bleibinhaus <stefan.bleibinhaus@gmail.com>
2015-01-25 21:54:10 -0800

    Add test with tuples to CustomIndexAnnotationTest

--- mau-annotation/src/test/scala/mau/annotation/CustomIndexAnnotationTest.scala
+++ mau-annotation/src/test/scala/mau/annotation/CustomIndexAnnotationTest.scala
@@ -13,7 +13,7 @@ class CustomIndexAnnotationTest extends MauRedisSpec("CustomIndexAnnotationTest"
       val person = Person(None, "Hans", 27)
       val savedPerson = await(personMauRepo.save(person))
       val id = savedPerson.id.get
-      val retrievedPeople = await(personMauRepo.findByFirstLetterOfName('H'))
+      val retrievedPeople = await(personMauRepo.findByFirstLetter('H'))
       retrievedPeople should be(Seq(savedPerson))
       val retrievedPerson = retrievedPeople(0)
       retrievedPerson.name should be(person.name)
@@ -24,7 +24,7 @@ class CustomIndexAnnotationTest extends MauRedisSpec("CustomIndexAnnotationTest"
       val person = Person(None, "Hans", 27)
       val savedPerson = await(personMauRepo.save(person))
       val id = savedPerson.id.get
-      val deleteResult = await(personMauRepo.deleteByFirstLetterOfName('H'))
+      val deleteResult = await(personMauRepo.deleteByFirstLetter('H'))
       deleteResult should be(1)
       val retrievedPerson = await(personMauRepo.get(id))
       retrievedPerson should be(None)
@@ -35,12 +35,25 @@ class CustomIndexAnnotationTest extends MauRedisSpec("CustomIndexAnnotationTest"
       val person = Person(None, "Hans", 27)
       val savedPerson = await(personMauRepo.save(person))
       val id = savedPerson.id.get
-      val countResult = await(personMauRepo.countByFirstLetterOfName('H'))
+      val countResult = await(personMauRepo.countByFirstLetter('H'))
       countResult should be(1)
     }
   }
 
-  @mauModel("Mau:Test:CustomIndexAnnotationTest", false)
+  describe("@customIndex annotation (tuple)") {
+    it("should allow to find instances") {
+      val personMauRepo = Person.mauRepo
+      val person = Person(None, "Hans", 27)
+      val savedPerson = await(personMauRepo.save(person))
+      val id = savedPerson.id.get
+      val retrievedPeople = await(personMauRepo.findByFirstLetterAge(('H', 27)))
+      retrievedPeople should be(Seq(savedPerson))
+      val retrievedPerson = retrievedPeople(0)
+      retrievedPerson.name should be(person.name)
+    }
+  }
+
+  @mauModel("Mau:Test:CustomIndexAnnotationTest", true)
   @sprayJson
   case class Person(
     id: Option[Id],
@@ -49,12 +62,20 @@ class CustomIndexAnnotationTest extends MauRedisSpec("CustomIndexAnnotationTest"
 
   object Person {
 
-    @customIndex("FirstLetterOfName")
-    private val firstLetterOfNameIndex = CustomIndexDeclaration[Person, Char](
+    @customIndex("FirstLetter")
+    private val firstLetterIndex = CustomIndexDeclaration[Person, Char](
       keySaveFunction =
         (person: Person) ⇒ Set(s"firstLetterOfName=${person.name.headOption.getOrElse("")}"),
       keyGetFunction =
         (char: Char) ⇒ Set(s"firstLetterOfName=$char")
+    )
+
+    @customIndex("FirstLetterAge")
+    private val firstLetterAgeIndex = CustomIndexDeclaration[Person, (Char, Int)](
+      keySaveFunction =
+        (person: Person) ⇒ Set(s"firstLetter=${person.name.headOption.getOrElse("")}:age=${person.age}"),
+      keyGetFunction =
+        (charAge: (Char, Int)) ⇒ Set(s"firstLetter=${charAge._1}:age=${charAge._2}")
     )
   }
 }
